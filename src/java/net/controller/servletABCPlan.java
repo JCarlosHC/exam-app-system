b3fa461f311815ce3aa4fea3d60426bedd29c395
Carlos <juanhernandezcastro.c@gmail.com>
2018-05-24 22:15:07 -0500

    The controllers-servlets was added

---
+++ src/java/net/controller/servletABCPlan.java
@@ -0,0 +1,125 @@
+package net.controller;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.dao.*;
+import net.model.planEstudio;
+
+public class servletABCPlan extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        //Recepcion de parametros
+        String action = request.getParameter("action");
+        String msgresult = request.getParameter("msg");
+        String success = request.getParameter("success");
+        String id = request.getParameter("idPlan");
+        String description = request.getParameter("description");
+        String startDate = request.getParameter("cStartDate");
+        String endDate = request.getParameter("cEndDate");
+
+        ConnectionDB cn = new ConnectionDB();
+        planEstudio pe = new planEstudio();
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        String msg = "", url = "";
+        boolean status = false;
+        RequestDispatcher rd;
+        planEstudioDAO peDao = new planEstudioDAO(cn);
+
+        //Crear mensaje
+        if (msgresult != null) {
+            request.setAttribute("msg", msgresult);
+            request.setAttribute("success", success);
+        }
+
+        switch (action) {
+            case "toList":
+                List<planEstudio> lista = peDao.getAll();
+                request.setAttribute("lista", lista);
+                url = "planEstudio.jsp";
+                break;
+            case "insert":
+                try {
+                    pe.setDescription(description);
+                    pe.setStartDate(formatDate.parse(startDate));
+                    pe.setEndDate(formatDate.parse(endDate));
+                    
+                    status = peDao.insert(pe);
+                    
+                    if (status) {
+                        msg = "Se guardo correctamente el plan de estudio";
+                    } else {
+                        msg = "Ocurrio un error, el plan de estudio no fue guardado";
+                    }
+                    
+                } catch (ParseException ex) {
+                    msg = ex.getMessage();
+                    status = false;
+                }   url = "servletABCPlan?action=toList&success=" + status + "&msg=" + msg;
+                break;
+            case "update":
+                try {
+                    pe.setId(Integer.parseInt(id));
+                    pe.setDescription(description);
+                    pe.setStartDate(formatDate.parse(startDate));
+                    pe.setEndDate(formatDate.parse(endDate));
+                    
+                    status = peDao.update(pe);
+                    
+                    if (status) {
+                        msg = "Se actualizo correctamente el plan de estudio";
+                    } else {
+                        msg = "Ocurrio un error, el plan de estudio no fue actualizado";
+                    }
+                    
+                } catch (ParseException ex) {
+                    msg = ex.getMessage();
+                    status = false;
+                }   url = "servletABCPlan?action=toList&success=" + status + "&msg=" + msg;
+                break;
+            case "delete":
+                try {
+                    status = peDao.delete(Integer.parseInt(id));
+                    
+                    if (status) {
+                        msg = "Se elimino correctamente el plan";
+                    } else {
+                        msg = "Ocurrio un error, el plan de estudio no se elimino";
+                    }
+                    
+                } catch (Exception ex) {
+                    msg = ex.getMessage();
+                    status = false;
+                }   url = "servletABCPlan?action=toList&success=" + status + "&msg=" + msg;
+                break;
+            default:
+                break;
+        }
+
+        cn.disconnect();
+        rd = request.getRequestDispatcher(url);
+        rd.forward(request, response);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
---
+++ src/java/net/controller/servletLogin.java
@@ -0,0 +1,76 @@
+package net.controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import net.dao.ConnectionDB;
+import net.dao.userDAO;
+import net.model.user;
+
+public class servletLogin extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String action = request.getParameter("action");
+        HttpSession sesion = request.getSession();
+        
+        if(action.equals("out")){
+            sesion.invalidate();
+            response.sendRedirect("login.jsp");
+        }else{
+           
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession sesion = request.getSession();
+        RequestDispatcher rd;
+        String usu = request.getParameter("email");
+        String pass = request.getParameter("password");
+        String action = request.getParameter("action");
+        String firstname = request.getParameter("firstname");
+        String psurname = request.getParameter("psurname");
+        String msurname = request.getParameter("msurname");
+        
+        ConnectionDB cn = new ConnectionDB();
+        userDAO usrDao = new userDAO(cn);
+        user usr = new user();
+        
+        if(action.equals("login")){
+            if(usrDao.validate(usu, pass) && sesion.getAttribute("userId") == null){
+                sesion.setAttribute("userId", usu);
+            }
+            response.sendRedirect("index.jsp");
+            
+        }else if(action.equals("register")){
+            usr.setFirstname(firstname);
+            usr.setPsurname(psurname);
+            usr.setMsurname(msurname);
+            usr.setEmail(usu);
+            usr.setPhone(null);
+            usr.setPassword(pass);
+            usr.setId_tipo(2); //Se crea por default con el tipo de profesor
+            usr.setId_status(1); //Se crea por default activo
+            
+            if(!usrDao.insert(usr)){
+                request.setAttribute("msg", "Ocurrio un error al registrar el usuario");
+            }
+            rd = request.getRequestDispatcher("login.jsp");
+            rd.forward(request, response);
+        }
+        
+        cn.disconnect();
+        
+        
+    }
+
+}
